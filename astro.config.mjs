b5cdcaf53837fdc54da5aa32c301e453e92c337e
omar1779 <josemyclans@gmail.com>
2024-11-13 16:18:25 -0500

    added new module netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),react()],
+  output: 'server',
+  adapter: netlify(),
 });
